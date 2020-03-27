f5ec069b0a0d4ad1dd2e12bf604d93dbad266379
shonenforcerpg <otgenaisis10@gmail.com>
2020-03-27 07:24:39 +0100

    upd

--- js/main.js
+++ js/main.js
@@ -29,3 +29,18 @@ function search_ninja() {
         }
     }
 }
+
+function search_wizard() {
+    let input = document.getElementById("searchbar3") .value 
+    input=input.toLowerCase();
+    let y=document.getElementsByClassName("wizard");
+
+    for (i=0; i<y.length; i++){
+        if(!y[i].innerHTML.toLowerCase().includes(input)){
+            y[i].style.display="none";
+        }
+        else{
+            y[i].style.display="list-item";
+        }
+    }
+}
